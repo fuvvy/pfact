0a9e992df04694f48ba5820b541a56da590fe79e
Mike Fusaro <fuvvy-github@nym.hush.com>
2017-06-26 11:21:52 -0400

    Minor improvments

--- PrimeTest.hs
+++ PrimeTest.hs
@@ -1,12 +1,14 @@
-module PrimeTest ( testFermat, odd ) where
+module PrimeTest ( primalityFermat, primalityMillerRabin ) where
 
 import Data.Maybe
-import Prelude
-import Debug.Trace
+--import Prelude
+--import Debug.Trace
 
 import SafeRand
 
-debug = (flip trace) False
+--debug = (flip trace) False
+
+data Primality = Composite | ProbablyPrime | Continue deriving (Show, Eq, Enum)
 
 fibsT n a b = 
   if n == 0
@@ -25,28 +27,28 @@ findCoprimeR n a =
 findCoprime :: Integral a => a -> a
 findCoprime n = findCoprimeR n . lcgLehmer $ n
 
-testFermat :: Integral t => t -> Bool
-testFermat n =
+primalityFermat :: Integral t => t -> Bool
+primalityFermat n =
   let a = findCoprime n
       r = a^(n-1) `mod` n
   in if r == 1
     then True
     else False
     
-testFibonacci :: Integral t => t -> Bool
-testFibonacci p =
+primalityFibonacci :: Integral t => t -> Bool
+primalityFibonacci p =
   let q = fibs p+1
       r = q `mod` p
   in if r == 0
     then True
     else False
     
-testPSW :: Integral t => t -> Bool
-testPSW p
+primalityPSW :: Integral t => t -> Bool
+primalityPSW p
   | e && f = True
   | otherwise = False
-  where e = testFermat p
-        f = testFibonacci p
+  where e = primalityFermat p
+        f = primalityFibonacci p
         
 isPrime :: Integral t => t -> Bool
 isPrime p
@@ -55,7 +57,7 @@ isPrime p
   | otherwise = False
   where o = odd p
         r = p `rem` 5
-        t = testPSW p
+        t = primalityPSW p
 
 getRD :: Integral t => t -> (t, t)
 getRD n = getRDT (n-1) 1
@@ -75,37 +77,35 @@ pickRangeInclusive a b s =
   in a + i
   
 -- Test with:
--- map testMillerRabin (take 50 $ filter odd [5..])
-testMillerRabin p = testMillerRabinR p 1337 10
+-- map primalityMillerRabin (take 50 $ filter odd [5..])
+primalityMillerRabin p = primalityMillerRabinR p 1337 10
 
---testMillerRabinR :: Int -> Bool
-testMillerRabinR p s k =
+--primalityMillerRabinR :: Int -> Bool
+primalityMillerRabinR p s k =
   let (r,d) = getRD p
-      l = witnessLoop p r d s k
-  in if l == False
-    then False
-    else True
+  in witnessLoop p r d s k
 
 --witnessLoop :: Integral t => t -> t -> t -> t -> t -> Bool
 witnessLoop n r d s k
 --  | debug ("witnessLoop " ++ show n ++ " " ++ show r ++ " " ++ show d ++ " " ++ show s ++ " " ++ show k) = False
-  | k == 0              = True
-  | x == 1 || x == n-1  = witnessLoop n r d (lcgLehmer s) (k-1)
-  | l == 0              = False
-  | l == 2              = False
-  | l == 1              = witnessLoop n r d (lcgLehmer s) (k-1)
-  | l == n-1            = witnessLoop n r d (lcgLehmer s) (k-1)
+--  | k == 0              = ProbablyPrime
+--  | x == 1 || x == n-1  = witnessLoop n r d t (k-1)
+--  | l == Composite      = Composite
+--  | l == Continue       = witnessLoop n r d t (k-1)
+  | k == 0                              = ProbablyPrime
+  | x == 1 || x == n-1 || l == Continue = witnessLoop n r d t (k-1)
+  | l == Composite                      = Composite
   where a = pickRangeInclusive 2 (n-2) s
         x = a^d `mod` n
+        t = lcgLehmer s
         l = innerLoop n x (r-1)
       
 --innerLoop :: Integral t => t -> t -> t -> t
 innerLoop n x r
 --  | debug ("innerLoop " ++ show n ++ " " ++ show x ++ " " ++ show r) = undefined
-  | y == 1    = 0
-  | y == n-1  = 1
-  | r == 0    = 2
-  | otherwise = innerLoop n y (r-1)
+  | r == 0 || y == 1  = Composite
+  | y == n-1          = Continue
+  | otherwise         = innerLoop n y (r-1)
   where y = x^2 `mod` n
   
 --rangeTest :: Integral t => t -> t -> String
