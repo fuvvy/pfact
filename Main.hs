f83593b3669d62dade2c99371ab11a1ac20a4ec8
Mike Fusaro <fuvvy-github@nym.hush.com>
2017-07-18 17:03:51 -0400

    code refactor

--- Main.hs
+++ Main.hs
@@ -26,24 +26,22 @@ options = Options {
   &= summary "pfact v0.1, Finds the prime decomposition of a given number."
   &= program "pfact"
 
-pretty' :: (Show a1, Show a, Ord a, Num a, Eq a1) => [a1] -> a -> String
-pretty' [x] 1 = show x
-pretty' [x] c = show x ++ "^" ++ show c
-pretty' (x:y:xs) c
-  | x /= y && c > 1 = show x ++ "^" ++ show c ++ " x " ++ pretty' (y:xs) 1
-  | x /= y = show x ++ " x " ++ pretty' (y:xs) 1
-  | otherwise = pretty' (y:xs) (c+1)
-
 pretty :: (Show a, Eq a, Num a) => [a] -> String
 pretty x
   | x == [0] || x == [1] = show x ++ " is neither prime nor composite and therefore has no prime factorization"
-  | otherwise = pretty' x 1
+  | otherwise = iter x 1
+  where
+    iter [x] 1 = show x
+    iter [x] c = show x ++ "^" ++ show c
+    iter (x:y:xs) c
+      | x /= y && c > 1 = show x ++ "^" ++ show c ++ " x " ++ iter (y:xs) 1
+      | x /= y = show x ++ " x " ++ iter (y:xs) 1
+      | otherwise = iter (y:xs) (c+1)
 
 main :: IO ()
 main = do
 --  Options{..} <- cmdArgs options
 --  putStrLn . pretty . pfact num $ seed
 --let p = 2^110503-1 in putStrLn . show . bitlen3 $ p
---putStrLn . show . lucasLehmer 110503 $ 5489439
-  putStrLn . show . lucasLehmer 9689 $ 5489439
+  putStrLn . show . lucasLehmer 110503 $ 5489439
 --  let p = 2^110503-1 in putStrLn . show . prime p $ 5489439
--- Primality.hs
+++ Primality.hs
@@ -6,9 +6,6 @@ import Data.Bits
 
 import SafeRand
 
-import Debug.Trace
-debug = (flip trace) False
-
 rounds = 1
 data Primality = Composite | ProbablyPrime | Prime | Continue deriving (Show, Eq, Enum)
 
@@ -17,25 +14,21 @@ data Primality = Composite | ProbablyPrime | Prime | Continue deriving (Show, Eq
 ----------------------------------------
 
 -- Fast, tail-recursive Fibonacci function
-fibs' :: Integer -> Integer -> Integer -> Integer
-fibs' n a b = 
-  if n == 0
-    then a
-    else fibs' (n-1) b $ a+b
-
 fibs :: Integer -> Integer
-fibs n = fibs' n 0 1
+fibs n = iter n 0 1 where
+  iter n a b = 
+    if n == 0
+      then a
+      else iter (n-1) b $ a+b
 
 -- Pseudorandom search for a coprime
-findCoprime' :: Integer -> Integer -> Integer
-findCoprime' n a = 
-  let g = gcd n a
-  in if g == 1
-    then a
-    else findCoprime' n . lcgLehmer $ a
-
 findCoprime :: Integer -> Integer
-findCoprime n = findCoprime' n . lcgLehmer $ n
+findCoprime n = iter n . lcgLehmer $ n where
+  iter n a = 
+    let g = gcd n a
+    in if g == 1
+      then a
+      else iter n . lcgLehmer $ a
 
 -- Fermat primality test
 -- 2^(p-1) = 1 (mod p)
@@ -69,83 +62,56 @@ pswTest p
 -- Lucas-Lehmer Mersenne primality test
 -------------------------------------------------
 
--- rolling our own integer log2 because Haskell's sucks
+-- rolling our own bit-length functions because Haskell's log2
+-- fails on large integers
 
--- Fastest
-bitlen' :: Integer -> Integer -> Integer
---bitlen' n a b | trace ("bitlen' " ++ show n ++ " " ++ show a) False = undefined
-bitlen' n a
-  | b == n = a+1
-  | b >  n = a
-  | b <  n = bitlen' n $ a+1
-  where b = shiftL 1 (fromIntegral a)
-
+-- Fastest - log log n solution - log2 (# of bits)
+-- recursivelly mask off log2(n) bits
 bitlen :: Integer -> Integer
-bitlen n = bitlen' n 1
+bitlen n = iter n 0 where
+  iter 1 0 = 1
+  iter n c
+    | scale c < n   = iter n $ c+1
+    | scale c == n  = 2^c+1
+    | scale c > n   = 2^(c-1) + iter (div n (2^2^(c-1))) 0
+    where scale f = shiftL 1 $ 2^f
+    
+-- Fast - test n against 2^i until overshoot then linear drift down
+bitlen_drift :: Integer -> Integer
+bitlen_drift n = up n 0 where
+  down n c =
+    if (shiftL 1 (fromIntegral c)) > n
+      then down n $ c-1
+      else c+1
+  up n c =
+    if (shiftL 1 (2^c)) < n
+      then up n $ c+1
+      else down n $ 2^c
 
---Faster
-bitlen_div' :: Integer -> Integer -> Integer
-bitlen_div' a b
-  | b == 1    = 0
-  | otherwise = 1 + bitlen_div' a (b `div` a)
+-- Middle of the road - linear in number of bits
+bitlen_lin :: Integer -> Integer
+bitlen_lin n = iter n 1 where
+  iter n a
+    | b == n = a+1
+    | b >  n = a
+    | b <  n = iter n $ a+1
+    where b = shiftL 1 (fromIntegral a)
 
+--Slow
 bitlen_div :: Integer -> Integer
-bitlen_div n = (bitlen_div' 2 n) + 1
+bitlen_div n = (iter 2 n) + 1 where
+  iter a b
+    | b == 1    = 0
+    | otherwise = 1 + iter a (b `div` a)
 
 --Slowest
-bitlen_exp' :: Integer -> Integer -> Integer
-bitlen_exp' n a
-  | b == n = a+1
-  | b >  n = a
-  | b <  n = bitlen_exp' n $ a+1
-  where b = 2^a
-
 bitlen_exp :: Integer -> Integer
-bitlen_exp n = bitlen_exp' n 1
-
-bitlen3 :: Integer -> Integer
-bitlen3 n = up n 0
-
-down :: Integer -> Integer -> Integer
-down n c =
-  if (shiftL 1 (fromIntegral c)) > n
-    then down n $ c-1
-    else c+1
-   
-up :: Integer -> Integer -> Integer
---up n c =
-up n c | trace ("up " ++ show n ++ " " ++ show c) False = undefined
-  | (shiftL 1 (2^c)) < n = up n $ c+1
-  | otherwise = down n $ 2^c
---  if (shiftL 1 (2^c)) < n
---    then up n $ c+1
---    else down n $ 2^c
-
-bitlen4 n = up2 n 0
-
---up2 n c
-up2 n c | trace ("up2 " ++ show n ++ " " ++ show c) False = undefined
-  | scale c < n && scale (c+1) > n  = (2^c) + up2 (div n (2^2^c)) 0
-  | scale c < n                     = up2 n $ c+1
-  | n < scale c                     = 1
-  | n == scale c                    = 2^c+1
-  where scale x = shiftL 1 (2^x)
-    
---upe n witness distance
-upe n witness distance | trace ("upe " ++ show n ++ " " ++ show witness ++ " " ++ show distance) False = undefined
---  | distance == 1 && test > n = 2^witness-1
-  | test < n  = upe n (witness+1) (distance+1)
-  | otherwise = downe n witness 0
-  where test = shiftL 1 (2^witness)
-  
---downe n witness distance
-downe n witness distance | trace ("downe " ++ show n ++ " " ++ show witness ++ " " ++ show distance) False = undefined
-  | distance == 1 && test (witness-1) < n = (2^witness)+1
-  | test witness > n  = downe n (witness-1) (distance+1)
-  | otherwise = upe n witness 0
-  where test w = shiftR (2^(2^w)) 1 
-  
-bitlene n = upe n 0 0
+bitlen_exp n = iter n 1 where
+  iter n a
+    | b == n = a+1
+    | b >  n = a
+    | b <  n = iter n $ a+1
+    where b = 2^a
 
 -- calculating (s^2 - 2) mod m repeatedly with gigantic moduli is slow
 -- fast mersenne mod trick: wikipedia.org/wiki/Lucas-Lehmer_primality_test#Time_complexity
@@ -154,7 +120,7 @@ fmmod k n
   | c <= n = k
   | g == m = 0
   | otherwise = fmmod ((k `rem` 2^n) + (k `div` 2^n)) n
-  where c = bitlen4 k
+  where c = bitlen k
         m = 2^n-1
         g = gcd k m
         
@@ -168,36 +134,33 @@ fmmod_bitwise k n
         g = gcd k m
         rest = shiftR k (fromInteger n)
         least = xor k (shiftL rest (fromInteger n))
-        
-lucasLehmer' :: Integer -> Integer -> Integer -> Integer
-lucasLehmer' s p c =
-  let s' = fmmod (s^2-2) p
-  in if c == 0
-    then s
-    else lucasLehmer' s' p $ c-1
 
 lucasLehmer :: Integer -> Integer -> Primality
 lucasLehmer p seed
   | prime p seed == Composite = Composite
   | s == 0 = Prime
   | s /= 0 = Composite
-  where s = lucasLehmer' 4 p $ p-2
+  where
+    s = iter 4 p $ p-2
+    iter s p c =
+      let s' = fmmod (s^2-2) p
+      in if c == 0
+        then s
+        else iter s' p $ c-1
         
 -------------------------------------------------
 -- Miller-Rabin Primality Test related functions
 -------------------------------------------------
 
 -- Every even integer can be written as 2^r*d where d is odd
-getRD' :: Integer -> Integer -> (Integer, Integer)
-getRD' n r =
-  let p = 2^r
-      d = quot n p
-  in if n `rem` p == 0 && odd d
-    then (r, d)
-    else getRD' n $ r+1
-
 getRD :: Integer -> (Integer, Integer)
-getRD n = getRD' n 1
+getRD n = iter n 1 where
+  iter n r =
+    let p = 2^r
+        d = quot n p
+    in if n `rem` p == 0 && odd d
+      then (r, d)
+      else iter n $ r+1
       
 innerLoop :: Integer -> Integer -> Integer -> Primality
 innerLoop n x c
@@ -215,16 +178,14 @@ witnessLoop n r d s c
         a = safeRange 2 (n-2) t
         x = modExp a d n
         l = innerLoop n x $ r-1
-
-millerRabin' :: Integer -> Integer -> Integer -> Primality
-millerRabin' p s c =
-  let (r,d) = getRD $ p-1
-  in witnessLoop p r d s c
   
 -- Test with:
 -- map primalityMillerRabin (take 50 $ filter odd [5..])
 millerRabin :: Integer -> Integer -> Primality
-millerRabin p s = millerRabin' p s rounds
+millerRabin p s = iter p s rounds where
+  iter p s c =
+    let (r,d) = getRD $ p-1
+    in witnessLoop p r d s c
   
 prime :: Integer -> Integer -> Primality
 prime p s = if even p then Composite else millerRabin p s
